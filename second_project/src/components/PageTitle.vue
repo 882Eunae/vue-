dbd16685d325eadddbdcc106a32697c4a1ece63f
882Eunae <pubao47@gmail.com>
2025-04-01 13:26:34 +0900

    커밋되라....

---
+++ second_project/src/components/PageTitle.vue
@@ -0,0 +1,11 @@
+<!--components/PageTitle.vue-->
+<template>
+  <h2>{{ title }}</h2>
+</template>
+
+<script>
+export default {
+  // 부모가 넘겨준 값들, Read Only
+  props: ['title'],
+}
+</script>
---
+++ second_project/src/components/PropsComponents.vue
@@ -0,0 +1,16 @@
+<!--components/PropsComponent.vue 부모-->
+<template>
+<div>
+  <PageTitle title="First!"/> 
+  <PageTitle title="Today is....."/>
+</div>  
+</template>
+
+<script>
+import PageTitle from './PageTitle.vue'; //자식
+export default{
+  components:{
+    PageTitle, //자식 컴포넌트 등록 
+  }
+}
+</script>
